128f3a89ce8cd37e58e1154f7b5e021fd0a45cee
crutchy <jared.crutchfield@hotmail.com>
2017-11-29 22:12:08 +0000

    changed XHR_PIPE_FILE to WEBSOCKET_XHR_PIPE_FILE in ws_app.php

--- ws_app.php
+++ ws_app.php
@@ -1,21 +1,19 @@
 <?php
 
-# this file is included by a webserver ajax stub handler script to write to the named pipe, which will then be read by the websocket server
+# this file is included by a webserver script to write to the named pipe, which will then be read by the websocket server
 # this script is not included by the websocket server itself
-
-define("XHR_PIPE_FILE","/var/include/vhosts/default/inc/data/ws_notify");
 
 #####################################################################################################
 
 function ws_pipe_data(&$params)
 {
-  if (file_exists(XHR_PIPE_FILE)==False)
+  if (file_exists(WEBSOCKET_XHR_PIPE_FILE)==False)
   {
     $params["pipe_status"]="PIPE FILE NOT FOUND";
   }
   else
   {
-    $xhr_pipe=fopen(XHR_PIPE_FILE,"w+");
+    $xhr_pipe=fopen(WEBSOCKET_XHR_PIPE_FILE,"w+");
     if ($xhr_pipe===False)
     {
       $params["pipe_status"]="ERROR OPENING PIPE FILE";
